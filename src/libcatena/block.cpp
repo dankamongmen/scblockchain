d6df30e8b74937ecdd70e92700f7a32c7d50d9c4
nick black <dankamongmen@gmail.com>
2018-11-30 00:33:08 -0500

    read blockfile, extract/validate headers

--- src/libcatena/block.cpp
+++ src/libcatena/block.cpp
@@ -15,10 +15,11 @@ int CatenaBlocks::verifyData(const char *data, unsigned len){
 		// FIXME free extracted blocks on any error (unique_ptr?)
 		if(len < BLOCKHEADERLEN){
 			std::cerr << "needed " << BLOCKHEADERLEN <<
-				"bytes, had only " << len << std::endl;
+				" bytes, had only " << len << std::endl;
 			return -1;
 		}
 		CatenaBlockHeader chdr;
+		memset(&chdr, 0, sizeof(chdr));
 		// FIXME could probably do this faster
 		std::memcpy(chdr.hash, data, sizeof(chdr.hash));
 		data += sizeof(chdr.hash);
@@ -31,14 +32,26 @@ int CatenaBlocks::verifyData(const char *data, unsigned len){
 			std::cerr << "expected version 0, got " << chdr.version << std::endl;
 			return -1;
 		}
-		std::memcpy(&chdr.totlen, data, 3);
-		if(chdr.totlen < BLOCKHEADERLEN){
+		for(int i = 0 ; i < 3 ; ++i){
+			chdr.totlen <<= 8;
+			chdr.totlen += *data++;
+		}
+		if(chdr.totlen < BLOCKHEADERLEN || chdr.totlen > len){
 			std::cerr << "invalid totlen " << chdr.totlen << std::endl;
 			return -1;
 		}
-		data += 3;
-		// FIXME copy remaining fields
+		for(int i = 0 ; i < 3 ; ++i){
+			chdr.txcount <<= 8;
+			chdr.txcount += *data++;
+		}
+		for(int i = 0 ; i < 40 ; ++i){
+			chdr.utc <<= 8;
+			chdr.utc += *data++;
+		}
+		// FIXME ensure reserved bytes are zeroes
+		// FIXME extract data section, stash block
 		len -= chdr.totlen;
+		++blocknum;
 	}
 	blockcount = blocknum;
 	return blockcount;
@@ -50,16 +63,11 @@ void CatenaBlocks::loadFile(const std::string& fname){
 	char *memblock;
 	std::streampos size;
 	f.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-	try{
-		f.open(fname, std::ios::in | std::ios::binary | std::ios::ate);
-		size = f.tellg();
-    		memblock = new char[size];
-		f.seekg(0, std::ios::beg);
-		f.read(memblock, size);
-	}catch(std::ifstream::failure& e){
-		std::cerr << "error reading file " << fname << std::endl;
-		throw;
-	}
+	f.open(fname, std::ios::in | std::ios::binary | std::ios::ate);
+	size = f.tellg();
+	memblock = new char[size];
+	f.seekg(0, std::ios::beg);
+	f.read(memblock, size);
 	int blocknum = verifyData(memblock, size);
 	if(blocknum <= 0){
 		return;
