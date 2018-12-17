6d54c4546a9b24db42e84ab6cb0465b9108c05e6
nick black <dankamongmen@gmail.com>
2018-12-17 14:59:11 -0500

    utility: extract HexOutput() from OStreamHash()

--- src/libcatena/utility.h
+++ src/libcatena/utility.h
@@ -1,6 +1,9 @@
 #ifndef CATENA_LIBCATENA_UTILITY
 #define CATENA_LIBCATENA_UTILITY
 
+#include <iomanip>
+#include <ostream>
+
 namespace Catena {
 
 inline unsigned long nbo_to_ulong(const unsigned char* data, int bytes){
@@ -10,6 +13,16 @@ inline unsigned long nbo_to_ulong(const unsigned char* data, int bytes){
 		ret += *data++;
 	}
 	return ret;
+}
+
+inline void HexOutput(std::ostream& s, const unsigned char* data, size_t len){
+	std::ios state(NULL);
+	state.copyfmt(s);
+	s << std::hex;
+	for(size_t i = 0 ; i < len ; ++i){
+		s << std::setfill('0') << std::setw(2) << (int)data[i];
+	}
+	s.copyfmt(state);
 }
 
 }
