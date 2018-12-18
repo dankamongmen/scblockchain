222b68a35ab30a3d3f4238ea595544969a62b885
nick black <dankamongmen@gmail.com>
2018-12-17 20:10:09 -0500

    Keypair: add == overload, HasPrivateKey()

--- src/libcatena/sig.h
+++ src/libcatena/sig.h
@@ -3,7 +3,7 @@
 
 // We use DER-encoded ECDSA secp256k1 curve for signatures
 
-#define SIGLEN 72 // length of signature outputs in bytes
+#define SIGLEN 72 // maximum length of signature outputs in bytes
 
 #include <openssl/evp.h>
 
@@ -28,6 +28,20 @@ Keypair(const Keypair& kp) :
 ~Keypair();
 size_t Sign(const unsigned char* in, size_t inlen, unsigned char* out, size_t outlen);
 bool Verify(const unsigned char* in, size_t inlen, const unsigned char* sig, size_t siglen);
+
+inline bool operator==(const Keypair& rhs) const {
+	if(EVP_PKEY_cmp(pubkey, rhs.pubkey) != 1){
+		return 0;
+	}
+	if(privkey && rhs.privkey){
+		return EVP_PKEY_cmp(privkey, rhs.privkey);
+	}
+	return 1;
+}
+
+bool HasPrivateKey() const {
+	return privkey != nullptr;
+}
 
 private:
 EVP_PKEY* pubkey;
