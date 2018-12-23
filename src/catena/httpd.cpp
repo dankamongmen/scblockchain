fa921a7569576a760fa6656dfa2b63c513673594
nick black <dankamongmen@gmail.com>
2018-12-22 21:43:48 -0500

    HTTPServer: use epoll, provide a basic handler

--- src/catena/httpd.cpp
+++ src/catena/httpd.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <microhttpd.h>
 #include <libcatena/chain.h>
 #include "catena/httpd.h"
@@ -9,19 +10,35 @@ int HTTPDServer::Handler(void* cls, struct MHD_Connection* conn, const char* url
 				const char* upload_data, size_t* upload_len,
 				void** conn_cls){
 	(void)cls;
-	(void)conn;
 	(void)url;
 	(void)method;
 	(void)version;
 	(void)upload_data;
 	(void)upload_len;
 	(void)conn_cls;
-	return MHD_HTTP_OK;
+	char buf[] = "<!DOCTYPE html><html lang=\"en\"><head><title>catena</title></head><body>heya</body></html>";
+	auto resp = MHD_create_response_from_buffer(strlen(buf), buf, MHD_RESPMEM_MUST_COPY);
+	if(resp == nullptr){
+		std::cerr << "couldn't create HTTP response" << std::endl;
+		// FIXME try to queue and send error
+		return MHD_NO;
+	}
+	if(MHD_NO == MHD_add_response_header(resp, MHD_HTTP_HEADER_CONTENT_TYPE, "text/html; charset=UTF-8")){
+		MHD_destroy_response(resp); // FIXME again, try to send error?
+		return MHD_NO;
+	}
+	auto ret = MHD_queue_response(conn, MHD_HTTP_OK, resp);
+	MHD_destroy_response(resp);
+	if(ret == MHD_NO){
+		std::cerr << "couldn't queue HTTP response" << std::endl;
+		return MHD_NO;
+	}
+	return ret;
 }
 
 HTTPDServer::HTTPDServer(Catena::Chain& chain, unsigned port) :
   chain(chain) {
-	unsigned flags = 0;
+	const unsigned flags = MHD_USE_SELECT_INTERNALLY | MHD_USE_EPOLL_LINUX_ONLY;
 	mhd = MHD_start_daemon(flags, port, NULL, NULL, Handler, NULL,
 				MHD_OPTION_END);
 	if(mhd == nullptr){
