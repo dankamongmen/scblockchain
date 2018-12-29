cc0cf5425659dac946a0a7dda8048aad1c92a256
nick black <dankamongmen@gmail.com>
2018-12-29 18:36:59 -0500

    Transaction: implement LookupAuthReqTX

--- src/libcatena/tx.h
+++ src/libcatena/tx.h
@@ -136,6 +136,38 @@ size_t GetPayloadLength() const {
 
 };
 
+class LookupAuthReqTX : public Transaction {
+public:
+LookupAuthReqTX() = default;
+LookupAuthReqTX(const CatenaHash& hash, unsigned idx) : Transaction(hash, idx) {}
+bool Extract(const unsigned char* data, unsigned len) override;
+bool Validate(TrustStore& tstore) override;
+std::ostream& TXOStream(std::ostream& s) const override;
+std::pair<std::unique_ptr<unsigned char[]>, size_t> Serialize() const override;
+nlohmann::json JSONify() const override;
+
+private:
+unsigned char signature[SIGLEN];
+
+// specifier of who signed this tx
+CatenaHash signerhash;
+uint32_t signeridx; // must be exactly 32 bits for serialization
+size_t siglen; // length of signature, up to SIGLEN
+std::unique_ptr<unsigned char[]> payload;
+size_t keylen; // length of public key
+size_t payloadlen; // total length of signed payload
+
+const unsigned char*
+GetJSONPayload() const {
+	return payload.get() + 32 + 4;
+}
+
+size_t GetJSONPayloadLength() const {
+	return payloadlen - 32 - 4;
+}
+
+};
+
 }
 
 #endif
