bbddf4860bd20cae1b24485c8c028262ec9551dd
nick black <dankamongmen@gmail.com>
2018-12-30 13:32:34 -0500

    PatientTX: implement Transaction interface

--- src/libcatena/patienttx.cpp
+++ src/libcatena/patienttx.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-#include <libcatena/externallookuptx.h>
+#include <libcatena/patienttx.h>
 #include <libcatena/utility.h>
 
 namespace Catena {
 
-bool ExternalLookupTX::Extract(const unsigned char* data, unsigned len) {
-	if(len < 2){ // 16-bit lookup length
-		std::cerr << "no room for lookup type in " << len << std::endl;
+bool PatientTX::Extract(const unsigned char* data, unsigned len) {
+	if(len < 2){ // 16-bit signature length
+		std::cerr << "no room for signature type in " << len << std::endl;
 		return true;
 	}
-	lookuptype = nbo_to_ulong(data, 2);
+	siglen = nbo_to_ulong(data, 2);
 	data += 2;
 	len -= 2;
 	if(len < signerhash.size() + sizeof(signeridx)){
@@ -22,13 +22,6 @@ bool ExternalLookupTX::Extract(const unsigned char* data, unsigned len) {
 	signeridx = nbo_to_ulong(data, sizeof(signeridx));
 	data += sizeof(signeridx);
 	len -= sizeof(signeridx);
-	if(len < 2){
-		std::cerr << "no room for siglen in " << len << std::endl;
-		return true;
-	}
-	siglen = nbo_to_ulong(data, 2);
-	data += 2;
-	len -= 2;
 	if(len < siglen || siglen > sizeof(signature)){
 		std::cerr << "no room for signature in " << len << std::endl;
 		return true;
@@ -45,56 +38,42 @@ bool ExternalLookupTX::Extract(const unsigned char* data, unsigned len) {
 		std::cerr << "no room for key in " << len << std::endl;
 		return true;
 	}
-	// FIXME verify that key is valid? verify payload is valid JSON?
-	// Key length is part of the signed payload, so don't advance data
+	// FIXME verify that key is valid?
 	payload = std::unique_ptr<unsigned char[]>(new unsigned char[len]);
 	memcpy(payload.get(), data, len);
 	payloadlen = len;
 	return false;
 }
 
-bool ExternalLookupTX::Validate(TrustStore& tstore) {
+bool PatientTX::Validate(TrustStore& tstore) {
 	if(tstore.Verify({signerhash, signeridx}, payload.get(),
 				payloadlen, signature, siglen)){
 		return true;
 	}
-	const unsigned char* data = payload.get();
-	size_t len = payloadlen;
-	uint16_t keylen;
-	if(len < sizeof(keylen)){
-		return true;
-	}
-	keylen = nbo_to_ulong(data, sizeof(keylen));
-	len -= sizeof(keylen);
-	data += sizeof(keylen);
-	std::array<unsigned char, sizeof(blockhash)> bhash;
-	bhash = blockhash;
-	Keypair kp(data, keylen);
-	tstore.addKey(&kp, {bhash, txidx});
+	// FIXME store metadata
 	return false;
 }
 
-std::ostream& ExternalLookupTX::TXOStream(std::ostream& s) const {
-	s << "ExternalLookup (type " << lookuptype << ", " << siglen
-		<< "b signature, " << payloadlen << "b payload, "
-		<< keylen << "b key)\n";
+std::ostream& PatientTX::TXOStream(std::ostream& s) const {
+	s << "Patient (" << siglen << "b signature, " << payloadlen
+		<< "b payload, " << keylen << "b key)\n";
 	s << " registrar: " << signerhash << "." << signeridx << "\n";
+	// FIXME payload is encrypted!
 	s << " payload: ";
 	std::copy(GetPayload(), GetPayload() + GetPayloadLength(), std::ostream_iterator<char>(s, ""));
 	return s;
 }
 
 std::pair<std::unique_ptr<unsigned char[]>, size_t>
-ExternalLookupTX::Serialize() const {
-	size_t len = 6 + siglen + signerhash.size() + sizeof(signeridx) +
-		payloadlen;
+PatientTX::Serialize() const {
+	size_t len = 4 + signerhash.size() + sizeof(signeridx) + siglen +
+		2 + keylen + payloadlen;
 	std::unique_ptr<unsigned char[]> ret(new unsigned char[len]);
-	auto data = TXType_to_nbo(TXTypes::ExternalLookup, ret.get());
-	data = ulong_to_nbo(lookuptype, data, 2);
+	auto data = TXType_to_nbo(TXTypes::Patient, ret.get());
+	data = ulong_to_nbo(siglen, data, 2);
 	memcpy(data, signerhash.data(), signerhash.size());
 	data += signerhash.size();
 	data = ulong_to_nbo(signeridx, data, 4);
-	data = ulong_to_nbo(siglen, data, 2);
 	memcpy(data, signature, siglen);
 	data += siglen;
 	memcpy(data, payload.get(), payloadlen);
@@ -102,15 +81,14 @@ ExternalLookupTX::Serialize() const {
 	return std::make_pair(std::move(ret), len);
 }
 
-nlohmann::json ExternalLookupTX::JSONify() const {
-	nlohmann::json ret({{"type", "ExternalLookup"}});
+nlohmann::json PatientTX::JSONify() const {
+	nlohmann::json ret({{"type", "Patient"}});
 	ret["sigbytes"] = siglen;
 	ret["signerhash"] = hashOString(signerhash);
 	ret["signeridx"] = signeridx;
 	ret["payload"] = std::string(reinterpret_cast<const char*>(GetPayload()), GetPayloadLength());
 	auto pubkey = std::string(reinterpret_cast<const char*>(GetPubKey()), keylen);
 	ret["pubkey"] = pubkey;
-	ret["subtype"] = lookuptype;
 	return ret;
 }
 
