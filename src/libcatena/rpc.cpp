6f7e9b6786c227955bda7081529b32e7042ea637
nick black <dankamongmen@gmail.com>
2019-01-09 00:13:55 -0500

    RPCService: bind listening sockets

--- src/libcatena/rpc.cpp
+++ src/libcatena/rpc.cpp
@@ -1,5 +1,7 @@
+#include <cstring>
 #include <sstream>
 #include <fstream>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <libcatena/utility.h>
 #include <libcatena/rpc.h>
@@ -14,9 +16,40 @@ void RPCService::OpenListeners() {
 	if(sd4 < 0){
 		throw NetworkException("couldn't get TCP/IPv4 socket");
 	}
+	struct sockaddr_in sin;
+	memset(&sin, 0, sizeof(sin));
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port);
+	sin.sin_addr.s_addr = INADDR_ANY;
+	if(bind(sd4, (const struct sockaddr*)&sin, sizeof(sin))){
+		close(sd4);
+		throw NetworkException("couldn't bind IPv4 listener");
+	}
 	sd6 = socket(AF_INET6, SOCK_STREAM | SOCK_CLOEXEC, 0);
 	if(sd6 < 0){
+		close(sd4);
 		throw NetworkException("couldn't get TCP/IPv6 socket");
+	}
+	struct sockaddr_in6 sin6;
+	memset(&sin6, 0, sizeof(sin6));
+	sin6.sin6_family = AF_INET6;
+	sin6.sin6_port = htons(port);
+	memcpy(&sin6.sin6_addr, &in6addr_any, sizeof(in6addr_any));
+	int v6only = 1;
+	if(setsockopt(sd6, IPPROTO_IPV6, IPV6_V6ONLY, &v6only, sizeof(v6only))){
+		close(sd6);
+		close(sd4);
+		throw NetworkException("no pure IPv6 listeners");
+	}
+	if(bind(sd6, (const struct sockaddr*)&sin6, sizeof(sin6))){
+		close(sd6);
+		close(sd4);
+		throw NetworkException("couldn't bind IPv6 listener");
+	}
+	if(listen(sd4, SOMAXCONN) || listen(sd6, SOMAXCONN)){
+		close(sd6);
+		close(sd4);
+		throw NetworkException("couldn't set up listener queues");
 	}
 }
 
@@ -42,8 +75,12 @@ RPCService::RPCService(Chain& ledger, int port, const std::string& chainfile) :
 RPCService::~RPCService() {
 	cancelled = true; // FIXME needs be at least an atomic
 	epoller.join();
-	close(sd4); // FIXME should only be done after Epoller is stopped?
-	close(sd6);
+	if(close(sd4)){
+		std::cerr << "warning: error closing ipv4 listening socket\n";
+	}
+	if(close(sd6)){
+		std::cerr << "warning: error closing ipv6 listening socket\n";
+	}
 }
 
 void RPCService::Epoller() {
