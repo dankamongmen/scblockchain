bd08fceb03e7d4e47d1bb237eb9c9dda5ae7d683
nick black <dankamongmen@gmail.com>
2019-01-10 17:21:45 -0500

    Update Peer unit tests for configuration bit

--- src/test/peer.cpp
+++ src/test/peer.cpp
@@ -8,33 +8,33 @@ static std::shared_ptr<Catena::SSLCtxRAII> GetSSLCTX() {
 
 TEST(CatenaPeer, Constructor){
 	auto sctx = GetSSLCTX();
-	Catena::Peer peer("127.0.0.1", 40404, sctx);
+	Catena::Peer peer("127.0.0.1", 40404, sctx, true);
 	EXPECT_EQ(peer.Port(), 40404);
-	Catena::Peer p1("127.0.0.1:80", 40404, sctx);
+	Catena::Peer p1("127.0.0.1:80", 40404, sctx, true);
 	EXPECT_EQ(p1.Port(), 80);
 }
 
 TEST(CatenaPeer, BadSyntax){
 	auto sctx = GetSSLCTX();
-	EXPECT_THROW(Catena::Peer("", 40404, sctx), Catena::ConvertInputException);
-	EXPECT_THROW(Catena::Peer("127.808.0.1", 40404, sctx), Catena::ConvertInputException);
-	EXPECT_THROW(Catena::Peer("  127.0.0.1", 40404, sctx), Catena::ConvertInputException);
-	EXPECT_THROW(Catena::Peer("m127.0.0.1", 40404, sctx), Catena::ConvertInputException);
-	EXPECT_THROW(Catena::Peer("127.0.0.1m", 40404, sctx), Catena::ConvertInputException);
-	EXPECT_THROW(Catena::Peer("127.0.0.1 ", 40404, sctx), Catena::ConvertInputException);
+	EXPECT_THROW(Catena::Peer("", 40404, sctx, true), Catena::ConvertInputException);
+	EXPECT_THROW(Catena::Peer("127.808.0.1", 40404, sctx, true), Catena::ConvertInputException);
+	EXPECT_THROW(Catena::Peer("  127.0.0.1", 40404, sctx, true), Catena::ConvertInputException);
+	EXPECT_THROW(Catena::Peer("m127.0.0.1", 40404, sctx, true), Catena::ConvertInputException);
+	EXPECT_THROW(Catena::Peer("127.0.0.1m", 40404, sctx, true), Catena::ConvertInputException);
+	EXPECT_THROW(Catena::Peer("127.0.0.1 ", 40404, sctx, true), Catena::ConvertInputException);
 }
 
 TEST(CatenaPeer, PeerBadPort){
 	auto sctx = GetSSLCTX();
-	EXPECT_THROW(Catena::Peer("127.0.0.1", 65536, sctx), Catena::ConvertInputException);
-	EXPECT_THROW(Catena::Peer("127.0.0.1", -1, sctx), Catena::ConvertInputException);
+	EXPECT_THROW(Catena::Peer("127.0.0.1", 65536, sctx, true), Catena::ConvertInputException);
+	EXPECT_THROW(Catena::Peer("127.0.0.1", -1, sctx, true), Catena::ConvertInputException);
 }
 
 TEST(CatenaPeer, PeerBadPortSyntax){
 	auto sctx = GetSSLCTX();
-	EXPECT_THROW(Catena::Peer("127.0.0.1:", 40404, sctx), Catena::ConvertInputException);
-	EXPECT_THROW(Catena::Peer("127.0.0.1:-1", 40404, sctx), Catena::ConvertInputException);
-	EXPECT_THROW(Catena::Peer("127.0.0.1:65536", 40404, sctx), Catena::ConvertInputException);
-	EXPECT_THROW(Catena::Peer("127.0.0.1:40404 ", 40404, sctx), Catena::ConvertInputException);
-	EXPECT_THROW(Catena::Peer("127.0.0.1: 40404", 40404, sctx), Catena::ConvertInputException);
+	EXPECT_THROW(Catena::Peer("127.0.0.1:", 40404, sctx, true), Catena::ConvertInputException);
+	EXPECT_THROW(Catena::Peer("127.0.0.1:-1", 40404, sctx, true), Catena::ConvertInputException);
+	EXPECT_THROW(Catena::Peer("127.0.0.1:65536", 40404, sctx, true), Catena::ConvertInputException);
+	EXPECT_THROW(Catena::Peer("127.0.0.1:40404 ", 40404, sctx, true), Catena::ConvertInputException);
+	EXPECT_THROW(Catena::Peer("127.0.0.1: 40404", 40404, sctx, true), Catena::ConvertInputException);
 }
