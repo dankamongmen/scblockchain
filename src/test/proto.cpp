6cddc44158c4d5ecad742e8a1feadb17be2bc1f2
nick black <dankamongmen@gmail.com>
2019-01-22 21:00:38 -0500

    Proto: add unit test using in-memory buffers

--- src/test/proto.cpp
+++ src/test/proto.cpp
@@ -3,11 +3,17 @@
 #include <capnp/serialize.h>
 #include <proto/catena.capnp.h>
 
-TEST(CatenaProto, AdvertiseNode){
-  ::capnp::MallocMessageBuilder message;
+static capnp::MallocMessageBuilder&
+AssembleAdvertiseNode(capnp::MallocMessageBuilder& message) {
   auto nodeAd = message.initRoot<Catena::Proto::AdvertiseNode>();
   auto ads = nodeAd.initAds(1);
   ads.set(0, "127.0.0.1:40404");
+  return message;
+}
+
+TEST(CatenaProto, AdvertiseNodeFd){
+  capnp::MallocMessageBuilder message;
+  AssembleAdvertiseNode(message);
   char tmpname[128] = "catenatest.XXXXXX";
   int fd = mkstemp(tmpname);
   ASSERT_GT(fd, 0);
@@ -18,4 +24,13 @@ TEST(CatenaProto, AdvertiseNode){
   auto rnodeAd = rmessage.getRoot<Catena::Proto::AdvertiseNode>();
   EXPECT_EQ(1, rnodeAd.getAds().size());
   close(fd);
+}
+
+TEST(CatenaProto, AdvertiseNodeFlatArray){
+  capnp::MallocMessageBuilder message;
+  AssembleAdvertiseNode(message);
+  kj::Array<capnp::word> words = capnp::messageToFlatArray(message);
+  capnp::FlatArrayMessageReader reader(words);
+  auto rnodeAd = reader.getRoot<Catena::Proto::AdvertiseNode>();
+  EXPECT_EQ(1, rnodeAd.getAds().size());
 }
