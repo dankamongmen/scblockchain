df7d13b63d3559fa8e38e70d0b705fa2e81652f3
nick black <dankamongmen@gmail.com>
2019-01-24 16:56:06 -0500

    PrepCall: move template to header

--- src/libcatena/proto.h
+++ src/libcatena/proto.h
@@ -2,12 +2,30 @@
 #define CATENA_LIBCATENA_PROTO
 
 #include <vector>
+#include <capnp/message.h>
+#include <capnp/rpc.capnp.h>
+#include <capnp/serialize.h>
+#include <proto/catena.capnp.h>
+#include <libcatena/proto.h>
 
 namespace Catena {
 
 template <typename T, typename F>
-std::vector<unsigned char> PrepCall(unsigned method,
-    const std::vector<unsigned char>& payload);
+std::vector<unsigned char> PrepCall(unsigned method, F fxn) {
+  capnp::MallocMessageBuilder builder;
+  auto call = builder.initRoot<capnp::rpc::Call>();
+  call.setMethodId(method);
+  auto pload = call.initParams();
+  auto anyp = pload.initContent();
+  typename T::Builder targbuilder = anyp.initAs<T>();
+  fxn(targbuilder);
+  auto words = capnp::messageToFlatArray(builder);
+  auto bytes = words.asBytes();
+  std::vector<unsigned char> ret;
+  ret.reserve(bytes.size());
+  ret.insert(ret.end(), bytes.begin(), bytes.end());
+  return ret;
+}
 
 }
 
